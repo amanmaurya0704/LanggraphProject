dd1d3b2f393b4e0c9082638ffa9f8866454de54f
amanmaurya0704 <aman.maurya0704@gmail.com>
2025-05-22 23:16:57 +0530

    new

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import os
 from datetime import date
-
+from src.langgraphagenticai.ui.streamlitui.uiconfigfile import Config
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 class LoadStreamlitUI:
     def __init__(self):
-        pass
+        self.config = Config()
+        self.user_control = {}
